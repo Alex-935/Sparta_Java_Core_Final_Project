a6947582ba8104972c7e34cca79cc31605e323b6
Alex-935 <alex.potter3@hotmail.co.uk>
2026-02-12 16:13:00 +0000

    Refomatted Deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -4,7 +4,7 @@ public class Deck {
 
         //public String[] deck;
         private List<Card> deck = new ArrayList();
-        private String[] suits = {"1", "2", "3", "4"};
+        private String[] suits = {"0", "1", "2", "3"};
         private String[] faces = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
 
         public Deck() {
