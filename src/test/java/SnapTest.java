bbfaec9900486f88f19fbe4c5523405834fc8f2e
Alex-935 <alex.potter3@hotmail.co.uk>
2026-02-13 14:15:54 +0000

    Updated tests

--- src/test/java/SnapTest.java
+++ src/test/java/SnapTest.java
@@ -3,4 +3,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 public class SnapTest {
+
+    @Test
+    @DisplayName("Check correct timer delay is set for 1")
+    void setDelayByChoice1Test() {
+        Assertions.assertTrue(SnapDelayTimer.setDelayByChoice(1) == 3500);
+    }
+
+    @Test
+    @DisplayName("Check correct timer delay is set for 2")
+    void setDelayByChoice2Test() {
+        Assertions.assertTrue(SnapDelayTimer.setDelayByChoice(2) == 2500);
+    }
+
+    @Test
+    @DisplayName("Check correct timer delay is set for 2")
+    void setDelayByChoice3Test() {
+        Assertions.assertTrue(SnapDelayTimer.setDelayByChoice(3) == 1500);
+    }
 }
